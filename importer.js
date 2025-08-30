a16bf4b3b53e407dc0c50f0f211da7245e30b883
ElPacer <pacenak@gmail.com>
2025-08-31 06:45:22 +1200

    Update importer.js

--- importer.js
+++ importer.js
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2025 Pacen Life Games. All rights reserved.
+ *
+ * This software and associated documentation files (the "Software") are the 
+ * confidential and proprietary information of Pacen Life Games ("Company"). 
+ * You shall not disclose or distribute the Software in whole or in part 
+ * without the prior written permission of the Company.
+ *
+ * The Software is provided "AS IS", without warranty of any kind, express or 
+ * implied, including but not limited to the warranties of merchantability, 
+ * fitness for a particular purpose, and noninfringement. In no event shall 
+ * the Company or its contributors be liable for any claim, damages, or other 
+ * liability, whether in an action of contract, tort, or otherwise, arising 
+ * from, out of, or in connection with the Software or the use or other 
+ * dealings in the Software.
+ */
+
 /* global TrelloPowerUp */
 const t = window.TrelloPowerUp.iframe();
 const logEl = document.getElementById('log');
